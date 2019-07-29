adb75b7ae5504267bf3f53497ba7089ddd82cd7b
followDev <followfashion312@gmail.com>
2019-07-29 16:57:25 +0200

    Remove unused file

--- theme/src/styles/device.tsx
+++
@@ -1,9 +0,0 @@
-import Theme from "./theme";
-
-export const device = {
-  xs: `(max-width: ${Theme.breakpoints.xs})`,
-  sm: `(max-width: ${Theme.breakpoints.sm})`,
-  md: `(max-width: ${Theme.breakpoints.md})`,
-  lg: `(max-width: ${Theme.breakpoints.lg})`,
-  xl: `(max-width: ${Theme.breakpoints.xl})`,
-};
